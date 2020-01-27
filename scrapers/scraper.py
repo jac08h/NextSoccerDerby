04e2fc2aa050d72eb3174d052a396f26dbf460ac
jac08h <jac08h@protonmail.com>
2020-01-27 16:48:16 +0100

    initial, not functioning version

---
+++ scrapers/scraper.py
@@ -0,0 +1,32 @@
+from typing import Dict
+import requests
+from bs4 import BeautifulSoup
+import logging
+import re
+
+logger = logging.getLogger(__name__)
+
+
+def scrape_wikipedia(wikipedia_url: str) -> Dict:
+    r = requests.get(wikipedia_url)
+    soup = BeautifulSoup(r.content, 'html.parser')
+    infobox = soup.find('table', {'class': 'infobox'})
+    next_meeting = None
+    for row in infobox.find_all_next('tr'):
+        if 'Next meeting' in row.text:
+            next_meeting = row.find('td')
+    if not next_meeting:
+        logger.error(f'No `Next meeting` info found. ({wikipedia_url},')
+        return {}  # raise exception
+    next_meeting_rows = next_meeting.childGenerator()
+
+
+if __name__ == '__main__':
+    urls = ['https://en.wikipedia.org/wiki/El_Cl%C3%A1sico',
+            'https://en.wikipedia.org/wiki/El_Viejo_Cl%C3%A1sico',
+            'https://en.wikipedia.org/wiki/Seville_derby',
+            'https://en.wikipedia.org/wiki/Athletic%E2%80%93Barcelona_cl%C3%A1sico']
+
+    for url in urls:
+        match_info = scrape_wikipedia(url)
+        print(match_info)
