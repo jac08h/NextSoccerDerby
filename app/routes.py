341ad9e28cae8935092f25700daa068e8a918cb9
jac08h <jac08h@protonmail.com>
2020-02-01 17:18:07 +0100

    show fixtures on webpage

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from app import app
+from flask import render_template
+from app import app, db
+from app.models import Fixture
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Next soccer derby"
+    fixtures = Fixture.query.all()
+    return render_template('index.html', title='Next Soccer Derby', fixtures=fixtures)
+
