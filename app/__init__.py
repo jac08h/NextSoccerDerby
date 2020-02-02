5d72470151f1dac89ea4a1451d3ab76812196cc9
jac08h <jac08h@protonmail.com>
2020-02-02 15:37:07 +0100

    users

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -11,7 +12,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 admin = Admin(app, name='nextsoccerderby')
-
+login = LoginManager(app)
 
 from app import routes, models
 admin.add_view(ModelView(models.Fixture, db.session))
