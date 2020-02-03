9b06843461b1e10d469393ef4e7e817a488f2779
jac08h <jac08h@protonmail.com>
2020-02-03 17:33:50 +0100

    database change
    changed env.py to use Batch migrations

--- app/models.py
+++ app/models.py
@@ -7,13 +7,13 @@ class Fixture(db.Model):
     __tablename__ = 'fixtures'
     id = db.Column(db.Integer, primary_key=True)
     wikipedia_url = db.Column(db.String(200), index=True, nullable=False, unique=True)
-    title = db.Column(db.String(100), index=True, nullable=False, unique=True)
-    country = db.Column(db.String(50), index=True, nullable=False, unique=False)
-    team_a = db.Column(db.String(100), index=True, nullable=False, unique=False)
-    team_b = db.Column(db.String(100), index=True, nullable=False, unique=False)
+    title = db.Column(db.String(100), index=True, unique=True)
+    country = db.Column(db.String(50), index=True, unique=False)
+    team_a = db.Column(db.String(100), index=True, unique=False)
+    team_b = db.Column(db.String(100), index=True, unique=False)
 
-    date = db.Column(db.DateTime, index=True, unique=False, nullable=True)
-    competition = db.Column(db.String(100), index=True, nullable=False, unique=False)
+    date = db.Column(db.DateTime, index=True, unique=False)
+    competition = db.Column(db.String(100), index=True, unique=False)
 
     def __repr__(self):
         return f"{self.title} - next match: {self.date}"
