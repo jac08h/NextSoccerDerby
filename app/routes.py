03394aad2042063176165fc5f54a3256d824167d
jac08h <jac08h@protonmail.com>
2020-03-14 19:22:50 +0100

    add /about page

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,11 @@ def index():
     return render_template('index.html', last_updated=last_updated)
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+
 @app.route('/faq')
 def faq():
     return render_template('faq.html', title='FAQ')
