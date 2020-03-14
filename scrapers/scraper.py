81a95dc1ea23ce6f3d7d183adc29b7c779087e1e
jac08h <jac08h@protonmail.com>
2020-03-14 11:34:55 +0100

    make scraper more readable, add new regex filter for unknown dates

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -11,16 +11,17 @@ class DateNotFound(Exception):
 
 
 def extract_data_from_wikipedia_page(wikipedia_url: str) -> Dict:
+    no_meeting_pattern = re.compile(r"unk. |TBA|TBD|To be confirmed")
+
     applogger.info(f'Scraping {wikipedia_url}')
     set_fields = {field: False for field in ['teams', 'competition', 'date']}
 
     r = requests.get(wikipedia_url)
     soup = BeautifulSoup(r.content, 'html.parser')
 
-    # Find Next meeting row
+    # Find `Next meeting` row
     infobox = soup.find('table', {'class': 'infobox'})
     found_next_meeting = False
-    no_meeting_pattern = re.compile(r"unk. |TBA")
     for row in infobox.find_all_next('tr'):
         if 'Next meeting' in row.text:
             found_next_meeting = True
@@ -29,43 +30,40 @@ def extract_data_from_wikipedia_page(wikipedia_url: str) -> Dict:
     if not found_next_meeting or re.search(no_meeting_pattern, next_meeting.text):
         raise DateNotFound
 
-    # Find teams names
     competition = date = team_a = team_b = None
-    """ 
-    try:
-        teams = next_meeting_rows[0].text  # as a 'Tag' element
-    except AttributeError:
-        teams = next_meeting_rows[0]  # as a Text
-    teams = teams.replace('v.', 'v')
-    if re.match(teams_pattern, teams):
-        team_a, team_b = [team.strip() for team in teams.split(' v ')]
-        teams_ok = True
-    """
 
     date_pattern = re.compile(r"\d\d? \w* \d{4}")
     teams_pattern = re.compile(r".+ v.? .+")
     for row in next_meeting_rows:
+        # get text content from row
+        try:
+            row_text = row.text  # as a 'Tag' element
+        except AttributeError:
+            row_text = row
+
+        # competition
         if row.name == 'a':
             competition_and_season = row.attrs['title']  # possible to use this in the future
             competition = row.text
             set_fields['competition'] = True
+            continue
 
-        else:
-            date_search = re.search(date_pattern, str(row))
-            if date_search:
-                date_string = date_search.group(0)
-                date = datetime.strptime(date_string, "%d %B %Y")
-                set_fields['date'] = True
+        # date
+        date_search = re.search(date_pattern, row_text)
+        if date_search:
+            date_string = date_search.group(0)
+            date = datetime.strptime(date_string, "%d %B %Y")
+            set_fields['date'] = True
+            continue
+        if re.search(no_meeting_pattern, row_text):  # no date found
+            raise DateNotFound
 
-            else:
-                try:
-                    teams = row.text  # as a 'Tag' element
-                except AttributeError:
-                    teams = row  # as a Text
-                teams = teams.replace('v.', 'v')
-                if re.match(teams_pattern, teams):
-                    team_a, team_b = [team.strip() for team in teams.split(' v ')]
-                    set_fields['teams'] = True
+        # teams
+        row_text = row_text.replace('v.', 'v')
+        if re.match(teams_pattern, row_text):
+            team_a, team_b = [team.strip() for team in row_text.split(' v ')]
+            set_fields['teams'] = True
+            continue
 
     if all(set_fields.values()):
         match_info = {'team_a': team_a, 'team_b': team_b, 'competition': competition, 'date': date}
