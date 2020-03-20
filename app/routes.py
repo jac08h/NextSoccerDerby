019f0cc3c58a58531de8ab7556709c15337c146c
jac08h <jac08h@protonmail.com>
2020-03-20 14:18:32 +0100

    Revert "show extra info on click"
    This reverts commit f2b0465002e2b938ce775dd4036b80535ee98aa2.

--- app/routes.py
+++ app/routes.py
@@ -48,12 +48,7 @@ def fixtures():
     for fixture in fixtures:
         if fixture.date is not None:  # don't display matches which are to be announced
             fixtures_data.append(
-                {'title': fixture.title,
-                 'date': fixture.get_date(),
-                 'teams': fixture.get_teams(),
-                 'country': fixture.get_country(),
-                 'competition': fixture.get_competition()
-                 })
+                (fixture.title, fixture.get_date(), fixture.get_competition(), fixture.get_teams(), fixture.get_country()))
 
         applogger.info('fixtures')
     return jsonify({"data": fixtures_data})
