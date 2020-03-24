6b92e7b1a1c795e75429b7ce1c684b2101f69071
jac08h <jac08h@protonmail.com>
2020-03-24 11:47:23 +0100

    move index javascript to separate file

---
+++ app/static/js/index.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+  $('#fixtures_table').DataTable({
+    "ajax": {
+      "url": "/fixtures",
+      "type": "POST"
+    },
+    "columns": [
+    { "data": "title"},
+    { "data": "date"},
+    { "data": "teams"},
+    { "data": "competition"},
+    { "data": "country"},
+    { "data": "wikipedia_url"},
+
+    { "data": "country"}  // placeholder
+    ],
+    "order": [
+      [1, "asc"]
+    ],
+    "responsive": {
+      "details": {
+        "renderer": function(api, rowIdx, columns) {
+          // Show hidden columns in row details
+          var data = $.map(columns, function(col, i) {
+            if(i == 6){ return; }
+            return col.hidden ?
+              '<tr><td>' + col.title + ':</td> ' +
+              '<td>' + col.data + '</td></tr>' :
+              '';
+          }).join('');
+
+          // Generate a table
+          data = $('<table width="100%"/>').append(data).prop('outerHTML');
+
+          return data;
+        }
+      }
+    },
+    "scrollY": "50vh",
+    "scrollCollapse": true,
+    "paging": false,
+    "columnDefs": [
+        // not displayed directly, only upon clicking on '+' button
+        {
+            "className": 'none',
+            "targets": [ 3, 4, 5 ]
+        },
+
+        // placeholder to always display '+' button
+        {
+            "className": 'none',
+            "targets": [ 6 ]
+        }
+    ]
+  });
+})
