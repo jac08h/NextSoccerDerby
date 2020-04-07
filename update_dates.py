733e378de3016170b933125ea228ead99c2ee496
jac08h <jac08h@protonmail.com>
2020-04-07 11:21:40 +0200

    don't display matches with dates which happened

--- update_dates.py
+++ update_dates.py
@@ -1,14 +1,22 @@
 from app import db, applogger, redis_client
 from app.models import Fixture
 from scrapers import scraper
-from datetime import datetime
+
+import datetime as dt
 from sys import exit
 
 fixtures = Fixture.query.all()
+today = dt.datetime.today()
+
+
 for fixture in fixtures:
     try:
         fixture_info = scraper.extract_data_from_wikipedia_page(fixture.wikipedia_url)
-        fixture.date = fixture_info['date']
+        if fixture_info['date'] - today < dt.timedelta(0):  # scraped date already happened
+            fixture.date = None
+        else:
+            fixture.date = fixture_info['date']
+
         fixture.competition = fixture_info['competition']
         fixture.team_a = fixture_info['team_a']
         fixture.team_b = fixture_info['team_b']
@@ -23,7 +31,7 @@ for fixture in fixtures:
 
     db.session.add(fixture)
 
-redis_client.set('last_updated', datetime.now().strftime('%B %-d, %Y'))
+redis_client.set('last_updated', dt.datetime.now().strftime('%B %-d, %Y'))
 # %-d represents a day without a leading zero, but it works only on UNIX systems
 # on Windows, %#d can be used
 db.session.commit()
