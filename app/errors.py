1cd962819f480e498a948bd580bb4a52521dabc6
jac08h <jac08h@protonmail.com>
2020-04-08 15:49:49 +0200

    add 403 error handler

--- app/errors.py
+++ app/errors.py
@@ -7,8 +7,11 @@ def forbidden_error(error):
     return render_template('403.html'), 403
 
 
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
 @app.errorhandler(500)
 def internal_server_error(error):
     return render_template('500.html'), 500
-
-
